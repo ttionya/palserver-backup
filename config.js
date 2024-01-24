2ae7719901833a7eb0479a7d132fabc5ee815c97
ttionya <git@ttionya.com>
2024-01-25 02:27:05 +0800

    fix: fix default cron

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
   TARGET_PATH: '', // E:/Steam/steamapps/common/PalServer/Pal/Saved/SaveGames/0/backup
   BIN_NAME: 'PalServer-Win64-Test-Cmd.exe',
   HEALTHCHECK_URL: '', // https://hc-ping.com/<your uuid>
-  HEALTHCHECK_CRON: '* */5 * * * *',
-  BACKUP_CRON: '* */10 * * * *',
+  HEALTHCHECK_CRON: '0 */5 * * * *',
+  BACKUP_CRON: '0 */10 * * * *',
   TIMEZONE: '',
 }
